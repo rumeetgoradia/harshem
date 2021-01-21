7cec431b44fa96d65469fbcd60ee8288dd06361f
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-01-21 00:15:32 -0500

    Test different branch names

--- components/Navbar/NavDrawer/NavDrawer.tsx
+++ components/Navbar/NavDrawer/NavDrawer.tsx
@@ -14,7 +14,7 @@ const NavDrawer: React.FC<NavDrawerProps> = ({ isOpen, onClose, children }) => {
 	return (
 		<Drawer isOpen={isOpen} placement="top" onClose={onClose}>
 			<DrawerOverlay>
-				<DrawerContent bg="brand.700" zIndex={9998} pt="88px" pb={6}>
+				<DrawerContent bg="brand.700" zIndex={9997} pt="88px" pb={6}>
 					<DrawerBody>{children}</DrawerBody>
 				</DrawerContent>
 			</DrawerOverlay>
