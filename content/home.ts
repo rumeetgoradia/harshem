246feccec6792ad9e242c8cfb06c256e30079557
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-04-05 20:36:07 -0400

    Fix typos

--- content/home.ts
+++ content/home.ts
@@ -1,9 +1,9 @@
-export const SUBTITLE = "Dr. Rita Goradia, MD PC"
+export const SUBTITLE = "Dr. Rita U. Goradia, MDPC"
 
 export const WHO_WE_ARE = [
 	"Harshem Family Practice is an outpatient medical practice for all primary care, geriatric, adolescent, and a majority of women’s health needs.  We have been serving patients in Central and Northern New Jersey since 2000 with the goal of providing high-quality modern medical care to maintain and improve overall patient health. We partner with our patients to provide effective management of chronic and acute conditions, all while empowering patients to play an active role in their own healthcare.",
-	"We at Harshem Family Practice offer a wide range of healthcare services that are preventative, diagnostic, and therapeutic to effectively treat a wide range of acute and chronic conditions. Our full range of services are designed to offer patients continuity of care in the outpatient, inpatient and nursing home settings.",
+	"We at Harshem Family Practice offer a wide range of healthcare services that are preventative, diagnostic, and therapeutic to effectively treat a wide range of acute and chronic conditions. Our full range of services are designed to offer patients continuity of care in the outpatient, inpatient, and nursing home settings.",
 ]
 
 export const MISSION =
-	"Our mission is to provide culturally competent, cost-effective, patient-centered, healthcare of the highest quality to a diverse group of patients."
+	"Our mission is to provide culturally competent, cost-effective, patient-centered healthcare of the highest quality to a diverse group of patients."
